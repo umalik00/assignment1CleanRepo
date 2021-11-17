d019cd56c29ce959059b3d80a9d388a78f05c3f2
rjasmine29 <jasmine.r@hotmail.com>
2021-11-17 12:56:49 +0000

    html css updated

---
+++ client/index.js
@@ -0,0 +1,14 @@
+//function myJavascriptFunction() {
+    // Your javascript code to bring data from database. 
+    
+  //}
+
+  document.addEventListener('DOMContentLoaded', () => {
+      console.log('Page Loaded')
+  })
+
+  let button = document.getElementById('button1') 
+button.onclick = () => {
+    console.log("button pressed")
+}
+
